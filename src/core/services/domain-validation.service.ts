126fd4c731f33d918c561d880139fd0d9bd04c99
Eduardo SG <eduardosgitp@gmail.com>
2025-08-01 02:37:55 -0600

    move role

--- src/core/services/domain-validation.service.ts
+++ src/core/services/domain-validation.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { User } from '@core/entities/user.entity';
 import { Role } from '@core/entities/role.entity';
