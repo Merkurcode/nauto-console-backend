fc284ad87d7588f87ed377af42f4d4658db17469
eduardosg98 <eduardosgitp@gmail.com>
2025-09-12 01:25:22 -0600

    removed unused handlers

--- src/queues/all/bulk-processing/bulk-processing.module.ts
+++ src/queues/all/bulk-processing/bulk-processing.module.ts
@@ -2,19 +2,10 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { BullModule } from '@nestjs/bullmq';
 
-import { BulkProcessingRequestCompletedHandler } from './event-handlers/bulk-processing-request-completed.handler';
-import { BulkProcessingRequestCreatedHandler } from './event-handlers/bulk-processing-request-created.handler';
-import { BulkProcessingRequestFailedHandler } from './event-handlers/bulk-processing-request-failed.handler';
 import { BulkProcessingProcessor } from './bulk-processing.processor';
 import { BulkProcessingHandlerService } from './bulk-processing-handler.service';
 import { BulkProcessingService } from '../../../core/services/bulk-processing.service';
 import { ModuleConfig } from './bulk-processing-config';
-
-const EventHandlers = [
-  BulkProcessingRequestCompletedHandler,
-  BulkProcessingRequestCreatedHandler,
-  BulkProcessingRequestFailedHandler,
-];
 
 @Module({
   imports: [
@@ -24,16 +15,10 @@ const EventHandlers = [
     }),
   ],
   providers: [
-    ...EventHandlers,
     BulkProcessingService, // <-- Declare first since processor depends on it
     BulkProcessingProcessor,
     BulkProcessingHandlerService,
   ],
-  exports: [
-    ...EventHandlers,
-    BulkProcessingProcessor,
-    BulkProcessingHandlerService,
-    BulkProcessingService,
-  ],
+  exports: [BulkProcessingProcessor, BulkProcessingHandlerService, BulkProcessingService],
 })
 export class BulkProcessingModule {}
